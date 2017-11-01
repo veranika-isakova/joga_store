93e9f4f84955bf51b22414be8c66bb931e1dd7e8
VERANIKA <veranika.isakova89@gmail.com>
2017-11-01 15:27:21 +0100

    Products and items

---
+++ joga_store.rb
@@ -0,0 +1,17 @@
+ shopping_cart = [ ]
+ @products = [
+  { reference_number: 1231, name: "Super Lite Mat", price: 10 },
+  { reference_number: 1232, name: "Power Mat", price: 20 },
+  { reference_number: 1233, name: "Block", price: 30 },
+  { reference_number: 1234, name: "Meditation cushion", price: 30 },
+  { reference_number: 1235, name: "The best T-shirt", price: 200 },
+  { reference_number: 1236, name: "The cutest yoga pants", price: 300 },
+  { reference_number: 1237, name: "Bring Yoga To Life", price: 30 },
+  { reference_number: 1238, name: "Light On Yoga", price: 10 }
+]
+
+puts "Welcome to our store!"
+sleep 2
+@products.each do |item|
+  puts "item: #{item[:name]} \n  reference_number: #{item[:reference_number]} \n price: #{item[:price]}"
+end
